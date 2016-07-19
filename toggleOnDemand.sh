13513ff6a4844b4b7b1f13df8eb6fba37f69a1c6
gnalbandian <gonzalonalbandian@hotmail.com>
2016-07-19 19:58:44 -0300

    Update toggleOnDemand.sh

--- toggleOnDemand.sh
+++ toggleOnDemand.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 export DISPLAY=:0
 
-Jivelite_pid=`pgrep -f jivelite`
-Epiphany_pid=`pgrep -f epiphany-browser`
-Jivelite_wid=`xdotool search --onlyvisible --pid $Jivelite_pid 2>/dev/null | head -n 1`
-Epiphany_wid=`xdotool search --onlyvisible --pid $Epiphany_pid 2>/dev/null | head -n 1`
-
-Active_wid=`xdotool getactivewindow`
-
-if [ $Active_wid -eq $Jivelite_wid ]
+if [[ $1 ]]
 then
-        xdotool windowactivate --sync $Epiphany_wid
+        Custom_pid=`pgrep $1`
+        Custom_wid=`xdotool search --onlyvisible --pid $Custom_pid 2>/dev/null | head -n 1`
+        xdotool windowactivate --sync $Custom_wid
 else
-        xdotool windowactivate --sync $Jivelite_wid
-fi
+        Jivelite_pid=`pgrep jivelite`
+        Epiphany_pid=`pgrep epiphany-browse`
+        Jivelite_wid=`xdotool search --onlyvisible --pid $Jivelite_pid 2>/dev/null | head -n 1`
+        Epiphany_wid=`xdotool search --onlyvisible --pid $Epiphany_pid 2>/dev/null | head -n 1`
 
+        Active_wid=`xdotool getactivewindow`
+        if [ $Active_wid -eq $Jivelite_wid ]
+        then
+                xdotool windowactivate --sync $Epiphany_wid
+        else
+                xdotool windowactivate --sync $Jivelite_wid
+        fi
+fi
 exit 0;

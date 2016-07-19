f8e4f77f4bffbb76cc4bda1850f0b4a4a91aa370
gnalbandian <gonzalonalbandian@hotmail.com>
2016-07-19 20:00:21 -0300

    Update taskMonitor.sh

--- taskMonitor.sh
+++ taskMonitor.sh
@@ -12,51 +12,51 @@ secondaryTasks="sshd wpa_supplicant"
 
 if [[ $1 == "-l" ]]
 then
-	echo -e "\e[4mCritical processes status\t[ Status ]\e[0m"
-	for criticalProcess in $criticalTasks
-	do
-		if [ `pgrep $criticalProcess` ]
-		then 
-			printf "%-32s  %-30s\n" "$criticalProcess" "${green}[ OK ]${normal}"
-		else 
-			printf "%-31s  %-31s\n" "$criticalProcess" "${red}[ NOK! ]${normal}"
-		fi
-	done
+        echo -e "\e[4mCritical processes status\t[ Status ]\e[0m"
+        for criticalProcess in $criticalTasks
+        do
+                if [ `pgrep $criticalProcess` ]
+                then
+                        printf "%-32s  %-30s\n" "$criticalProcess" "${green}[ OK ]${normal}"
+                else
+                        printf "%-31s  %-31s\n" "$criticalProcess" "${red}[ NOK! ]${normal}"
+                fi
+        done
 
-	echo -e "\n\e[4mPrimary tasks status\t\t[ Status ]\e[0m"
-	
-	for primaryProcess in $primaryTasks
-	do
-		if [ `pgrep $primaryProcess` ]
-		then 
-			printf "%-32s  %-30s\n" "$primaryProcess" "${green}[ OK ]${normal}"
-		else 
-			printf "%-31s  %-31s\n" "$primaryProcess" "${red}[ NOK! ]${normal}"
-		fi
-	done	
+        echo -e "\n\e[4mPrimary tasks status\t\t[ Status ]\e[0m"
+
+        for primaryProcess in $primaryTasks
+        do
+                if [ `pgrep $primaryProcess` ]
+                then
+                        printf "%-32s  %-30s\n" "$primaryProcess" "${green}[ OK ]${normal}"
+                else
+                        printf "%-31s  %-31s\n" "$primaryProcess" "${red}[ NOK! ]${normal}"
+                fi
+        done
 else
-	while [ 1 ]
-	do
-		if [ `awk -F. '{print $1}' /proc/uptime` -gt 60 ]
-		then
-			for criticalProcess in $criticalTasks
-			do
-				if [ `pgrep $criticalProcess || echo 0` -eq 0 ]
-					then
-						echo -e "`date +'%d/%m/%Y %H:%M'`: $criticalProcess \e[91mDOWN\e[0m. Rebooting system.\n" >> /home/pi/taskMonitor.log
-						sudo reboot
-					fi
-			done
-	
-			for primaryProcess in $primaryTasks
-			do
-				if [ `pgrep $primaryProcess || echo 0` -eq 0 ]
-				then
-					sudo systemctl restart $primaryProcess.service
-					/bin/echo -e "`date +'%d/%m/%Y %H:%M'`: $primaryProcess \e[91mDOWN\e[0m. Restarting service.\n"  >> /home/pi/taskMonitor.log
-				fi
-			done
-		fi
-		sleep $interval
-	done
+        while [ 1 ]
+        do
+                if [ `awk -F. '{print $1}' /proc/uptime` -gt 60 ]
+                then
+                        for criticalProcess in $criticalTasks
+                        do
+                                if [ `pgrep $criticalProcess || echo 0` -eq 0 ]
+                                        then
+                                                echo -e "`date +'%d/%m/%Y %H:%M'`: $criticalProcess \e[91mDOWN\e[0m. Rebooting system.\n" >> /home/pi/taskMonitor.log
+                                                sudo reboot
+                                        fi
+                        done
+
+                        for primaryProcess in $primaryTasks
+                        do
+                                if [ `pgrep $primaryProcess || echo 0` -eq 0 ]
+                                then
+                                        sudo systemctl restart $primaryProcess.service
+                                        /bin/echo -e "`date +'%d/%m/%Y %H:%M'`: $primaryProcess \e[91mDOWN\e[0m. Restarting service.\n"  >> /home/pi/taskMonitor.log
+                                fi
+                        done
+                fi
+                sleep $interval
+        done
 fi
